d2c139a50125c133db619f1f4546c06038a97edd
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-10-06 16:20:23 -0600

    minor update to text

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -10,7 +10,7 @@ export default function ResourcesPage() {
   return (
     <>
     <h1 className="resource-header-h">Resources</h1>
-    <p className="resource-header-p">One of the most difficult questions when approaching a topic is this: "Where in the world do I look? Where can I look? What can I trust?" Whether new to a field and trying to get your bearings or someone who has been around the block and is looking to expand their repertoire, this is for you. Below is a collection of resources for a wide variety of topics within the field. 
+    <p className="resource-header-p">One of the most difficult questions when approaching a topic is this: "Where in the world do I look? Where can I look? What can I trust?" Whether you are new to a field and trying to get your bearings or someone who has been around the block and is looking to expand their repertoire, this is for you. Below is a collection of resources for a wide variety of topics within the field. 
     <br />
     If you see something that is missing or know of something that should be added, <a href="">submit an update</a> and we'll make sure it gets added. </p>
     <hr className="horizontal-row" />
